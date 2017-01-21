4cabb7dc5f6397edf5e1d0fc798f9878e9ad2da0
Saran Yamasathien <pom.saran@gmail.com>
2017-01-21 09:40:55 +0700

    Chapter 46

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
